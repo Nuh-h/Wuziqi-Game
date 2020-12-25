05286c1b96356f54cb8c39d7cf0f34a025e2546f
Nuh <69435206+Nuh-h@users.noreply.github.com>
2020-12-25 18:14:49 +0000

    Update app.js

--- app.js
+++ app.js
@@ -161,7 +161,7 @@ document.querySelector('.feedbackBtn').addEventListener('click',launchSwalForm)
 async function launchSwalForm(){
 	const { value: val } = await Swal.fire({
 		title: 'Feedback',
-		html: '<form class="feedbackForm" name="Feedback Form" method="POST" data-netlify="true"><input type="hidden" name="form-name" value="Feedback Form"/><input type="text" id="name" name="Sender\'s name" class="swal2-input" placeholder="Your name"><textarea id="feedback" name="Sender\'s message" class="swal2-input" placeholder="Your feedback..."></textarea></form>',
+		html: '<form class="feedbackForm" name="Feedback Form" method="POST" data-netlify="true"><input type="hidden" name="form-name" value="Feedback Form"/><input type="text" id="name" name="name" class="swal2-input" placeholder="Your name"><textarea id="feedback" name="message" class="swal2-input" placeholder="Your feedback..."></textarea></form>',
 		showCancelButton: true,
 		confirmButtonText: "Submit"//,
 		//preConfirm: 
@@ -193,7 +193,6 @@ async function launchSwalForm(){
 		  icon:'success',
 		  text:'Thank you very much '+swal.getHtmlContainer().querySelector('#name').value.trim()+' for your feedback!!'
 	  });
-	  if(finish){ document.body.querySelector('.hidden').appendChild(feedback);
-	  setTimeout(feedback.submit(),500);}
+	  if(finish){ setTimeout(feedback.submit(),500);}
 	}
 };
